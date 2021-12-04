372b518e37d391bf84cc3d45923b7725b209d370
PanicS7 <55251860+PanicS7@users.noreply.github.com>
2021-12-04 20:40:59 +0100

    Update token.js

--- src/token.js
+++ src/token.js
@@ -1,3 +1,3 @@
-var privateToken = "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"; // replace this 
+var privateToken = "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"; // replace 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 with your token
 
 export default privateToken;
